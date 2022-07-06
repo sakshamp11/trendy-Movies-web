979824d51b413e852c6cf0e4b9f98e1a38b2856a
sakshamp11 <rimpi.saksham@gmail.com>
2022-07-06 19:35:35 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+
+import NavBar from "./components/NavBar";
+import Banner from "./components/Banner";
+import MovieList from "./components/MovieList";
+import Favourites from "./components/Favourites";
+
+import {
+  BrowserRouter,Routes,Route,} from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <NavBar />
+
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <>
+              <Banner /> 
+              <MovieList />
+            </>
+          }
+        />
+        <Route path="/favourites" element={<Favourites />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ src/components/Banner.js
@@ -0,0 +1,55 @@
+import React, { Component } from 'react'
+import { matchRoutes } from 'react-router-dom'
+import {movies} from '../MovieData'
+import axios from "axios";
+let s=""
+export class Banner extends Component {
+  constructor() {
+    console.log("construtor first");
+    super();
+
+    this.state = {
+      
+      movieset: [],
+      currPage: Math.floor(Math.random()*54)+1,
+     
+    };
+  }
+  async componentDidMount() {
+    const res = await axios.get(
+      `https://api.themoviedb.org/3/movie/popular?api_key=0b5415eb9bf023d556ef265b425e0e4a&language=en-US&page=${this.state.currPage}`
+    );
+    let movieData = res.data;
+    console.log(movieData);
+
+    this.setState({
+      movieset: [...movieData.results],
+    });
+
+    console.log("mounting done with CDM third");
+  }
+  change=async()=> {
+    movies=[...this.state.movieset]
+    
+
+  }
+  render() {
+    this.change()
+    let moviesElem=movies.results[Math.floor(Math.random()*11)+2]
+    let backDrop =  moviesElem.backdrop_path
+    let namemov=moviesElem.title
+    return (
+
+        <div className="banner-card" >
+        <img src={`https://image.tmdb.org/t/p/original${backDrop}`} className="card-img-top banner-img" alt="..."/>
+       
+          <h5 className="banner-title" >{namemov}</h5>
+          {/* <p className="banner-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+          <a href="#" className="btn btn-primary">Go somewhere</a> */}
+        </div>
+      
+    )
+  }
+}
+
+export default Banner
